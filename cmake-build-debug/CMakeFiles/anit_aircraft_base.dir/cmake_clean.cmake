45fb79a32d6bdc74f9f680cb60059eb8ed7e8e9c
Daniel <recondrone7@gmail.com>
2019-01-14 17:11:39 +0100

    project initialization

---
+++ cmake-build-debug/CMakeFiles/anit_aircraft_base.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/anit_aircraft_base.dir/main.cpp.obj"
+  "anit_aircraft_base.pdb"
+  "anit_aircraft_base.exe"
+  "anit_aircraft_base.exe.manifest"
+  "libanit_aircraft_base.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/anit_aircraft_base.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
