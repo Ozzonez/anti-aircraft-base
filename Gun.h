a26940378ae54553e4ec955902806c700b0dead8
Daniel <recondrone7@gmail.com>
2019-01-14 19:16:40 +0100

    added virtual class

---
+++ Gun.h
@@ -0,0 +1,8 @@
+#ifndef ANIT_AIRCRAFT_BASE_GUN_H
+#define ANIT_AIRCRAFT_BASE_GUN_H
+class Gun
+{
+    virtual void askToShoot()=0;
+    virtual void reload()=0;
+};
+#endif //ANIT_AIRCRAFT_BASE_GUN_H
